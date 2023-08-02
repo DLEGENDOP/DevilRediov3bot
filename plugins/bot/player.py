a9c583342e14492465577755f2e7e0b7143366b7
DLEGENDOP <140735285+DLEGENDOP@users.noreply.github.com>
2023-08-02 23:40:32 +0530

    Update player.py

--- plugins/bot/player.py
+++ plugins/bot/player.py
@@ -566,12 +566,12 @@ allcmd = ["play", "current", "playlist", "song", f"song@{USERNAME}", f"play@{USE
 async def not_chat(_, m: Message):
     buttons = [
             [
-                InlineKeyboardButton("CHANNEL", url="https://t.me/AsmSafone"),
-                InlineKeyboardButton("SUPPORT", url="https://t.me/AsmSupport"),
+                InlineKeyboardButton("CHANNEL", url="https://t.me/Classics0012"),
+                InlineKeyboardButton("SUPPORT", url="https://t.me/Classics002"),
             ],
             [
-                InlineKeyboardButton("🤖 MAKE YOUR OWN BOT 🤖", url="https://heroku.com/deploy?template=https://github.com/AsmSafone/RadioPlayerV3"),
+                InlineKeyboardButton("🤖 MAKE YOUR OWN BOT 🤖", url="https://heroku.com/deploy?template=https://te.legra.ph/file/70111b0bc46fddf36333f.mp4"),
             ]
          ]
-    k=await m.reply_photo(photo="https://telegra.ph/file/4e839766d45935998e9c6.jpg", caption="**Sorry, You Can't Use This Bot In This Group! 🤷‍♂️ But You Can Make Your Own Bot Like This From The [Source Code](https://github.com/AsmSafone/RadioPlayerV3) Below 😉!**", reply_markup=InlineKeyboardMarkup(buttons))
+    k=await m.reply_photo(photo="https://graph.org/file/ac6b9ad447404863e32bd.jpg", caption="**Sorry, You Can't Use This Bot In This Group! 🤷‍♂️ But You Can Make Your Own Bot Like This From The [Source Code](https://te.legra.ph/file/70111b0bc46fddf36333f.mp4) Below 😉!**", reply_markup=InlineKeyboardMarkup(buttons))
     await mp.delete(m)
